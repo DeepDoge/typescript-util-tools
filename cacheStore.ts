b5a5135bb3e5baef7e37bb05d5382e9d41681b87
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-07-14 06:50:00 +0000

    ...

---
+++ cacheStore.ts
@@ -0,0 +1,92 @@
+export function createCacheStore<T>(storeName: string, expireTime: number = Number.MAX_SAFE_INTEGER)
+{
+    let db: IDBDatabase | null = null
+    const openRequest = indexedDB.open(storeName)
+    openRequest.addEventListener('upgradeneeded', () => openRequest.result.createObjectStore(storeName).createIndex("expireAt", "expireAt"))
+    openRequest.addEventListener('success', () =>
+    {
+        db = openRequest.result
+        clearExpired()
+    })
+
+    async function getAwaitDB()
+    {
+        while (!db) new Promise((r) => setTimeout(r, 100))
+        return db
+    }
+
+
+    async function clearExpired()
+    {
+        return new Promise<void>(async (resolve, reject) =>
+        {
+            const transaction = (await getAwaitDB()).transaction(storeName, "readwrite")
+            const range = IDBKeyRange.upperBound(new Date())
+
+            const expireAtCursorRequest = transaction.objectStore(storeName).index("expireAt").openCursor(range)
+            expireAtCursorRequest.addEventListener('error', () => reject(expireAtCursorRequest.error))
+            expireAtCursorRequest.addEventListener('success', () =>
+            {
+                try
+                {
+                    const expireCursor = expireAtCursorRequest.result
+                    if (!expireCursor) return
+                    expireCursor.delete()
+                    expireCursor.continue()
+                    resolve()
+                }
+                catch (ex)
+                {
+                    reject(ex)
+                }
+            })
+        })
+    }
+
+    async function clearAll()
+    {
+        return await new Promise<void>(async (resolve, reject) =>
+        {
+            const store = (await getAwaitDB()).transaction(storeName, "readwrite").objectStore(storeName)
+            
+            const request = store.clear()
+            request.addEventListener('success', () => resolve())
+            request.addEventListener('error', () => reject(request.error))
+        })
+    }
+
+    async function put(key: string, value: T): Promise<void>
+    {
+        return await new Promise(async (resolve, reject) =>
+        {
+            const store = (await getAwaitDB()).transaction(storeName, "readwrite").objectStore(storeName)
+
+            const expireAt = new Date(Date.now() + expireTime)
+            const request = store.put({ value: value, expireAt }, key)
+            request.addEventListener('success', () => resolve())
+            request.addEventListener('error', () => reject(request.error))
+        })
+    }
+
+    async function get(id: string): Promise<T | undefined>
+    {
+        const response = (await new Promise(async (resolve, reject) =>
+        {
+            const store = (await getAwaitDB()).transaction(storeName, "readonly").objectStore(storeName)
+
+            const request = store.get(id)
+            request.addEventListener('success', () => resolve(request.result))
+            request.addEventListener('error', () => reject(request.error))
+        }) as { value: T, expireAt: Date } | undefined)
+
+        if (response === undefined) return undefined
+        if (response.expireAt <= new Date())
+        {
+            await clearExpired()
+            return undefined
+        }
+        return response.value
+    }
+
+    return { put, get, clearAll }
+}
